895daafb19ebcf1bd4ffd32357508e91fbb9cea8
ianm101 <ianmurray2019@gmail.com>
2021-05-30 11:28:35 -0400

    Adding uncropped receipt images

---
+++ preprocessing.py
@@ -0,0 +1,15 @@
+from scikit-image import io, color
+
+from scipy.fftpack import dct, idct
+
+img = io.imread(<image file>)
+
+gray = color.rgb2gray(img)
+
+frequencies = dct(dct(gray, axis=0), axis=1)
+
+frequencies[:2,:2] = 0
+
+gray = idct(idct(frequencies, axis=1), axis=0)
+
+gray = (gray - gray.min()) / (gray.max() - gray.min()) # renormalize to range [0:1]
